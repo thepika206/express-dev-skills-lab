8c3d3bcc861ccab5dde1636b806227e4a630ca95
pcheung <pika206@gmail.com>
2022-09-20 16:05:07 -0500

    add data directory and skill-data.js, add view/skills/index.ejs

---
+++ data/skill-data.js
@@ -0,0 +1,11 @@
+const skills = [
+  {name: 'JavaScript', acquired: true, _id: 125223},
+  {name: 'HTML', acquired: true, _id: 125221},
+  {name: 'CSS', acquired: true, _id: 125222},
+  {name: 'NodeJS', acquired: true, _id: 125224},
+  {name: 'ExpressJS', acquired: false, _id: 125225},
+]
+
+export { 
+	skills
+}
