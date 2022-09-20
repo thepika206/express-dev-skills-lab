2a842ed7a7518251b9ea49562fe5ffb071a32c44
pcheung <pika206@gmail.com>
2022-09-20 17:38:38 -0500

    new models directory and skill model

---
+++ models/skill.js
@@ -0,0 +1,17 @@
+import mongoose from 'mongoose'
+
+// optional shortcut to the mongoose.Schema class
+const Schema = mongoose.Schema
+	
+const skillSchema = new Schema({
+  name: String,
+  acquired: Boolean,
+})
+
+// Compile the schema into a model and export it
+
+const Skill = mongoose.model('Skill', skillSchema)
+
+export {
+  Skill
+}
